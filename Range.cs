cbb31600a4c60b63d0db75cfef6daa6d8f4e257a
Wnntyl <otsegolectr@gmail.com>
2024-09-06 10:09:34 +0400

    Range struct

---
+++ Range.cs
@@ -0,0 +1,14 @@
+using System;
+
+[Serializable]
+public struct Range<T>
+{
+    public T Min;
+    public T Max;
+
+    public Range(T min, T max)
+    {
+        Min = min;
+        Max = max;
+    }
+}
